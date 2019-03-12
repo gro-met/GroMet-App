1db0804d0e4593a44788f769b2546fadf17db377
Cooper Healy <mhealy@mst.edu>
2019-03-12 13:44:54 -0500

    Plant data should only have 10 points

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import PlotPage from './Plot'
+import PlotPage from './PlotPage'
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => PlotPage);
