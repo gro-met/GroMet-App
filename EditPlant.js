e2f84aa9cbde38bb7956b8b0f1d0721d02727658
Anna Pankiewicz <anna.pankiewicz13@gmail.com>
2019-04-18 16:15:23 -0500

    Add choosing/uploading image functions

--- EditPlant.js
+++ EditPlant.js
@@ -1,15 +1,54 @@
 import React, {Component} from "react";
-import { View, FlatList, List, Text, Image, StyleSheet, TextInput, TouchableOpacity, Alert, Picker } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
 const { Map } = require('immutable');
 import firebase from 'firebase';
 import ModalSelector from 'react-native-modal-selector';
+import ImagePicker from 'react-native-image-picker'
+import RNFetchBlob from 'rn-fetch-blob';
+
+const Blob = RNFetchBlob.polyfill.Blob;
+const fs = RNFetchBlob.fs;
+window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
+window.Blob = Blob;
+
+const uploadImage = (uri, mime = 'application/octet-stream') => {
+  return new Promise((resolve, reject) => {
+    const uploadUri = Platform.OS === 'ios'? uri.replace('file://', '') : uri
+    const sessionId = new Date().getTime()
+    let BlobUpload = null
+    const imageRef = storage.ref('images').child(`${sessionId}`)
+
+    fs.readFile(uploadUri, 'base64')
+    .then((data) => {
+      return Blob.build(data, { type: `${mime};BASE64`} )
+    })
+    .then((blob) => {
+      BlobUpload = blob
+      return imageRef.put(blob, {contentType: mime })
+    })
+    .then(() => {
+      BlobUpload.close()
+      return imageRef.getDownloadURL()
+    })
+    .then((url) => {
+      resolve(url)
+    })
+    .catch((error) => {
+      reject(error)
+    })
+  })
+}
 
 export default class EditPlant extends React.Component {
-  state = {
-    newName: '',
-    newSpecies: ''
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      newName: '',
+      newSpecies: ''
+    }
   }
+
   handleName = (text) => {
       this.setState({ newName: text })
   }
@@ -27,6 +66,15 @@ export default class EditPlant extends React.Component {
       ],
       {cancelable: false},
     );
+  }
+  chooseImage() {
+    this.setState({ uploadURL: '' })
+
+    ImagePicker.launchImageLibrary({}, response => {
+      uploadImage(response.uri)
+        .then(url => this.setState({ uploadURL: url} ))
+        .catch(error => console.log(error))
+    })
   }
 
   static navigationOptions = ({ navigation }) => {
