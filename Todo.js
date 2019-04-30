6bfa8f3054db8b76d243623cb3821db5bdecb5ee
Luttrell, Shelby A.(S&T-Student) <salhx9@mst.edu>
2019-04-30 17:08:48 -0500

    checkboxes do not have labels yet

--- Todo.js
+++ Todo.js
@@ -1,15 +1,21 @@
 import React, {Component} from "react";
 import { View, FlatList, List, Text, Image, StyleSheet, TextInput, TouchableOpacity, Alert, Picker } from 'react-native';
-import { ListItem, CheckBox } from 'react-native-elements';
+//import { CheckBox, ListItem} from 'react-native-elements';
+import CheckBox from 'react-native-check-box';
 const { Map } = require('immutable');
 import firebase from 'firebase';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 export default class Todo extends React.Component {
-    // state =  {
-    //     titleText = "Todo List for Plant",
-    //     speciesText = this.props.navigation.getParam('species','info')
-    // };
+    constructor(props){
+        super(props);
+        this.state={
+          isChecked1:false,
+          isChecked2:false
+        //   titleText = this.props.navigation.getParam('title', 'Info'),
+        //   speciesText = this.props.navigation.getParam('species', 'Info')
+        }
+      }
 
     static navigationOptions = ({ navigation }) => {
         return {
@@ -25,14 +31,47 @@ export default class Todo extends React.Component {
     };
 
     render(){
-        const { navigation } = this.props;
+        //const { navigation } = this.props;
         return(
-            <View style = {styles.container}>
-              <Text>titleText</Text>
-              <Text>speciesText</Text>
+            <View 
+                style = {styles.container}>
+                {/* <Text>{titleText}</Text>
+                <Text>{speciesText}</Text> */}
+                {/* <Text>titleText</Text>
+                <Text>speciesText</Text> */}
+                <View>
                 {/* <CheckBox
-                title='Click Here'
-                checked={this.state.checked}/> */}
+                    center
+                    title='Click Here'
+                    checkedIcon='dot-circle-o'
+                    uncheckedIcon='circle-o'
+                    checked={this.state.checked}
+                    onClick={()=>{
+                        this.setState({
+                            isChecked:!this.state.isChecked
+                        })
+                    }}
+                /> */}
+                    <CheckBox
+                        style={{flex: 10, padding: 50}}
+                        onClick={()=>{
+                        this.setState({
+                            isChecked1:!this.state.isChecked1
+                        })
+                        }}
+                        isChecked={this.state.isChecked1}
+                    />
+                    <CheckBox
+                        
+                        style={{flex: 10, padding: 50}}
+                        onClick={()=>{
+                        this.setState({
+                            isChecked2:!this.state.isChecked2
+                        })
+                        }}
+                        isChecked={this.state.isChecked2}
+                    />
+                </View>
             </View>
         );
     }
