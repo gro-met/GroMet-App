611c6dcb0f51ff320b6460184b9967bca501ec99
Anna Pankiewicz <Anna.pankiewicz13@gmail.com>
2019-05-02 16:25:27 -0500

    annapankiewicz/image upload (#17)
    * Add choosing/uploading image functions
    
    * Add correct permissions for image uploading
    
    * Remove camera permissions
    
    Currently we're just allowing the user to upload a photo that's already
    been taken, not to take a picture through the app for immediate upload.
    
    * Add photo uploading
    
    * Change method of launching image picker
    
    * Revert "Remove camera permissions"
    
    This reverts commit 03e41b8b84f347108c385918d9182aea2098773e.
    
    * Upload photo to firebase
    
    The images are successfully uploaded to Firebase storage with this commit.
    
    * Update firebase to use the new image URL
    
    * Add correct iOS permissions

--- EditPlant.js
+++ EditPlant.js
@@ -1,23 +1,65 @@
 import React, {Component} from "react";
-import { View, FlatList, List, Text, Image, StyleSheet, TextInput, TouchableOpacity, Alert, Picker } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert, Platform } from 'react-native';
 const { Map } = require('immutable');
 import firebase from 'firebase';
 import ModalSelector from 'react-native-modal-selector';
+import ImagePicker from 'react-native-image-picker'
+import RNFetchBlob from 'rn-fetch-blob';
+import { firebaseApp } from './App.js';
+
+const Blob = RNFetchBlob.polyfill.Blob;
+const fs = RNFetchBlob.fs;
+window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
+window.Blob = Blob;
+
+const uploadImage = (uri, mime = 'application/octet-stream') => {
+  return new Promise((resolve, reject) => {
+    const uploadUri = Platform.OS === 'ios'? uri.replace('file://', '') : uri
+    const sessionId = new Date().getTime()
+    let uploadBlob = null
+    const imageRef = firebaseApp.storage().ref('images').child(`${sessionId}`)
+
+    fs.readFile(uploadUri, 'base64')
+    .then((data) => {
+      return Blob.build(data, { type: `${mime};BASE64`} )
+    })
+    .then((blob) => {
+      uploadBlob = blob
+      return imageRef.put(blob, { contentType: mime })
+    })
+    .then(() => {
+      uploadBlob.close()
+      return imageRef.getDownloadURL()
+    })
+    .then((url) => {
+      resolve(url)
+    })
+    .catch((error) => {
+      reject(error)
+    })
+  })
+}
 
 export default class EditPlant extends React.Component {
-  state = {
-    newName: '',
-    newSpecies: ''
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      newName: '',
+      newSpecies: '',
+      uploadURL: '',
+    }
   }
+
   handleName = (text) => {
       this.setState({ newName: text })
   }
-  changeInfo = (newName, newSpecies) => {
+  changeInfo = (newName, newSpecies, newImageURL) => {
     key = this.props.navigation.getParam('key', 'Info')
     firebase.database().ref('/info/' + key.id).update({
       name: newName,
-      species: newSpecies
+      species: newSpecies,
+      img: newImageURL,
     });
     Alert.alert(
       'Plant Data Changed',
@@ -27,6 +69,27 @@ export default class EditPlant extends React.Component {
       ],
       {cancelable: false},
     );
+  }
+  chooseImage() {
+    this.setState({ uploadURL: '' })
+
+    ImagePicker.showImagePicker({}, response => {
+      console.log('Response: ', response);
+
+      if(response.didCancel) {
+        console.log('User cancelled picker');
+      }
+      else if(response.error) {
+        console.log('ImagePicker error: ', response.error);
+      }
+      else if(response.customButton) {
+        console.log('Pressed custom button: ', response.customButton);
+      }
+
+      uploadImage(response.uri)
+        .then(url => this.setState({ uploadURL: url} ))
+        .catch(error => console.log(error))
+    })
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -78,9 +141,17 @@ export default class EditPlant extends React.Component {
         </ModalSelector>
 
         <TouchableOpacity
+          style = {styles.photoButton}
+          onPress = {
+            () => this.chooseImage()
+          }>
+          <Text style = {styles.submitButtonText}> Choose Image </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
             style = {styles.submitButton}
             onPress = {
-            () => this.changeInfo(this.state.newName, this.state.newSpecies)
+            () => this.changeInfo(this.state.newName, this.state.newSpecies, this.state.uploadURL)
             }>
             <Text style = {styles.submitButtonText}> Submit Changes </Text>
         </TouchableOpacity>
@@ -98,6 +169,12 @@ const styles = StyleSheet.create({
       height: 40,
       borderColor: '#13771b',
       borderWidth: 1
+   },
+   photoButton: {
+      backgroundColor: '#4a9b51',
+      padding: 10,
+      margin: 15,
+      height: 40,
    },
    submitButton: {
       backgroundColor: '#13771b',
