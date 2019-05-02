b34afc84dfe937f5e797b82cc1fc2a1f6b6bef0c
Luttrell, Shelby A.(S&T-Student) <salhx9@mst.edu>
2019-05-01 20:07:05 -0500

    checkboxes work

--- Todo.js
+++ Todo.js
@@ -1,19 +1,24 @@
 import React, {Component} from "react";
 import { View, FlatList, List, Text, Image, StyleSheet, TextInput, TouchableOpacity, Alert, Picker } from 'react-native';
-//import { CheckBox, ListItem} from 'react-native-elements';
-import CheckBox from 'react-native-check-box';
+import CheckBox from 'react-native-checkbox';
+//import CheckBox from 'react-native-check-box';
 const { Map } = require('immutable');
 import firebase from 'firebase';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 export default class Todo extends React.Component {
+    //this constructor changes the filling of the checklist
     constructor(props){
         super(props);
         this.state={
           isChecked1:false,
-          isChecked2:false
-        //   titleText = this.props.navigation.getParam('title', 'Info'),
-        //   speciesText = this.props.navigation.getParam('species', 'Info')
+          isChecked2:false,
+          isChecked3:false,
+          stringy:"Have you loved me?",
+          watered:"Have I been watered?",
+          appearsHealthy:"Am I healthy?",
+          titleText: this.props.navigation.getParam('title', 'Info'),
+          speciesText:this.props.navigation.getParam('species', 'Info')
         }
       }
 
@@ -35,25 +40,12 @@ export default class Todo extends React.Component {
         return(
             <View 
                 style = {styles.container}>
-                {/* <Text>{titleText}</Text>
-                <Text>{speciesText}</Text> */}
-                {/* <Text>titleText</Text>
-                <Text>speciesText</Text> */}
+                <Text>{this.state.titleText} your {this.state.speciesText} </Text>
+                <Text> needs these things.</Text>
                 <View>
-                {/* <CheckBox
-                    center
-                    title='Click Here'
-                    checkedIcon='dot-circle-o'
-                    uncheckedIcon='circle-o'
-                    checked={this.state.checked}
-                    onClick={()=>{
-                        this.setState({
-                            isChecked:!this.state.isChecked
-                        })
-                    }}
-                /> */}
                     <CheckBox
-                        style={{flex: 10, padding: 50}}
+                        label={this.state.watered}
+                        style={{flex: 50, padding: 50}}
                         onClick={()=>{
                         this.setState({
                             isChecked1:!this.state.isChecked1
@@ -62,14 +54,24 @@ export default class Todo extends React.Component {
                         isChecked={this.state.isChecked1}
                     />
                     <CheckBox
-                        
-                        style={{flex: 10, padding: 50}}
+                        label={this.state.appearsHealthy}
+                        style={{flex: 50, padding: 50}}
                         onClick={()=>{
                         this.setState({
                             isChecked2:!this.state.isChecked2
                         })
                         }}
                         isChecked={this.state.isChecked2}
+                    />
+                    <CheckBox
+                        label={this.state.stringy}
+                        style={{flex: 50, padding: 50}}
+                        onClick={()=>{
+                        this.setState({
+                            isChecked3:!this.state.isChecked3
+                        })
+                        }}
+                        isChecked={this.state.isChecked3}
                     />
                 </View>
             </View>
@@ -79,21 +81,15 @@ export default class Todo extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-       paddingTop: 23
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
     },
     input: {
        margin: 15,
        height: 40,
        borderColor: '#13771b',
        borderWidth: 1
-    },
-    submitButton: {
-       backgroundColor: '#13771b',
-       padding: 10,
-       margin: 15,
-       height: 40,
-    },
-    submitButtonText:{
-       color: 'white'
     }
  })
